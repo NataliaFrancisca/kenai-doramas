39754e2c6ca27937ca7ddb353f85c953ab214b76
NataliaFrancisca <nathifrancisca@gmail.com>
2022-04-06 16:07:13 -0300

    update db

--- src/db/data.js
+++ src/db/data.js
@@ -6,24 +6,73 @@ export const db = [
         note: 8.8,
         description: "The absolute top secret love story of a chaebol heiress who made an emergency landing in North Korea because of a paragliding accident and a North Korean special officer who falls in love with her and who is hiding and protecting her.",
         image: null,
-        watch: [{netflix: "https://www.netflix.com/br-en/title/81159258"}]
+        watch: "https://www.netflix.com/br-en/title/81159258"
     },
 
     {
         id: 1,
-        title: "Crash Landing on You",
-        category: ["adventure", "comedy", "romance"],
-        note: 8.8,
-        description: "The absolute top secret love story of a chaebol heiress who made an emergency landing in North Korea because of a paragliding accident and a North Korean special officer who falls in love with her and who is hiding and protecting her.",
-        image: "",
-        watch: [
-            {
-            netflix: "https://www.netflix.com/br-en/title/81159258",
-            amazon: null, 
-            disney: null,
-            hbo: null}
-        ]
+        title: "Memories of the Alhambra",
+        category: ["Drama", "Romance", "Sci-fi"],
+        note: 7.9,
+        description: "After suffering a setback following a friend's betrayal Yoo Jin Woo travels to Spain on a business. There, he stays at an old hostel owned by a former classical guitarist Jung Hee Joo. The two get entangled in a mysterious incident.",
+        image: "img/memories-of-alhambra-poster.jpg",
+        watch: "https://www.netflix.com/br-en/title/81004280"
     },
+
+    {
+        id: 2,
+        title: "Guardian: The Lonely and Great God (Goblin)",
+        category: ["Comedy", "Drama", "Fantasy"],
+        note: 8.7,
+        description: "In his quest for a bride to break his immortal curse, Dokkaebi, a 939-year-old guardian of souls, meets a grim reaper and a sprightly student with a tragic past.",
+        image: "img/goblin-poster.jpg",
+        watch: "https://www.amazon.com/Goblin-Lonely-Great-God-Season/dp/B078KKX2VC"
+    },
+
+    {
+        id: 3,
+        title: "Start-Up",
+        category: ["Comedy", "Drama", "Romance"],
+        note: 8.2,
+        description: "Young entrepreneurs aspiring to launch virtual dreams into reality compete for success and love in the cutthroat world of Korea's high-tech industry.",
+        image: "img/start-up-poster.jpg",
+        watch: "https://www.netflix.com/br-en/title/81290293"
+    },
+
+    {
+        id: 4,
+        title: "Beating Again",
+        category: ["Comedy", "Drama", "Romance"],
+        note: 7.7,
+        description: "Kang Min Ho is young man who want to revenge his uncle's unjust takeover of his father's company. Then he needs to undergo a heart transplant surgery. After the surgery, he completely changes and begins to learn the true meaning of love.",
+        image: "img/beating-again-poster.jpg",
+        watch: "https://www.viki.com/tv/26491c-falling-for-innocence"
+    },
+
+    {
+        id: 5,
+        title: "Juvenile Justice",
+        category: ["Crime", "Drama"],
+        note: 8.1,
+        description: "A prickly judge who despises juvenile criminals arrives at a juvenile court, where she takes on a complex role examining punishment for young offenders.",
+        image: "img/juvenile-justice-poster.jpg",
+        watch: "https://www.netflix.com/br-en/title/81312802?source=35"
+    },
+
+    {
+        id: 6,
+        title: "Squid Game",
+        category: ["Action", "Drama", "Mystery"],
+        note: 8.0,
+        description: "Hundreds of cash-strapped players accept a strange invitation to compete in children's games. Inside, a tempting prize awaits with deadly high stakes. A survival game that has a whopping 45.6 billion-won prize at stake.",
+        image: "img/squid-game.jpg",
+        watch: "https://www.netflix.com/br-en/title/81290293"
+    },
+
+
+
+
+
 ]
 
 
