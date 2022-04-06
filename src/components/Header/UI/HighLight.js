cc37208084039ec7fe8a4cc89ba97d10c8fc0830
NataliaFrancisca <nathifrancisca@gmail.com>
2022-04-06 12:59:50 -0300

    creating the highlight component

---
+++ src/components/Header/UI/HighLight.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { db } from "../../../db/data";
+
+const HighLight = ({className}) => {
+
+    const dataHeader = db.find(element => element.id == 0)
+    
+    return(
+        <section className={className}>
+            <div className="header-category-list">
+                {dataHeader.category.map(category => (
+                    <span className="category">{category}</span>
+                ))}
+            </div>
+
+            <div className="stars">
+                <img src="img/icons/star.png" />
+                <img src="img/icons/star.png" />
+                <img src="img/icons/star.png" />
+                <img src="img/icons/star.png" />
+                </div>
+
+            <h1>{dataHeader.title}</h1>
+
+            <p>{dataHeader.description}</p>
+
+            <button>Asssistir Agora</button>
+        </section>
+    )
+}
+
+export default HighLight;
