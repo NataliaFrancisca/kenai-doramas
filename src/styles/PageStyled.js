8747b2fc43691f3a920a9d8ee513e3bedd6e3280
NataliaFrancisca <nathifrancisca@gmail.com>
2022-04-07 13:58:42 -0300

    removing the attachment

--- src/styles/PageStyled.js
+++ src/styles/PageStyled.js
@@ -12,4 +12,13 @@ export const PageStyled = styled.main`
         background-size: cover;
         background-position: center;
     }
+
+    @media only screen and (max-width: 700px) {
+        background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 1)), url(img/serie-pousando-no-amor.jpg);
+        background-size: cover;
+        background-position: left;
+        /* background-position: center;
+        background-attachment: fixed; */
+    }
+    
 `
