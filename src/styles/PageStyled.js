467f3013e183e6f9f8b20d06cecee66e7f6e2f35
NataliaFrancisca <nathifrancisca@gmail.com>
2022-04-07 11:46:11 -0300

    changing the gradient background

--- src/styles/PageStyled.js
+++ src/styles/PageStyled.js
@@ -4,7 +4,7 @@ export const PageStyled = styled.main`
     background-color: black;
     display: flex;
     flex-direction: column;
-    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)),url(img/pousando-no-amor.jpg);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 1)),url(img/pousando-no-amor.jpg);
     background-repeat: no-repeat;
     background-size: contain;
 `
