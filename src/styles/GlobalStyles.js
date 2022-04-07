c1d10b61b7fdbb70cbfffdb7f2c2c147da00fad2
NataliaFrancisca <nathifrancisca@gmail.com>
2022-04-07 12:25:17 -0300

    fixing the problem that create a white margin

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -8,4 +8,10 @@ export const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
     }
 
+    html,body{
+        width: 100%;
+        height: 100%;
+        overflow-x: hidden; 
+    }
+
 `
