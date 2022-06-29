f51c172423e43b4b2f36262549f969d092ba10a4
NataliaFrancisca <nathifrancisca@gmail.com>
2022-06-29 12:14:21 -0300

    feat: use svg icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,12 @@ import React from "react";
 const Footer = ({className}) => {
     return(
         <footer className={className}>
-
             <h1>Kenai</h1>
-
             <nav>
                 <ul>
-                    <li><img src="img/icons/instagram.png"></img></li>
-                    <li><img src="img/icons/twitter.png"></img></li>
-                    <li><img src="img/icons/facebook.png"></img></li>
+                    <li><img src="img/icons/instagram.svg"></img></li>
+                    <li><img src="img/icons/twitter.svg"></img></li>
+                    <li><img src="img/icons/facebook.svg"></img></li>
                 </ul>
             </nav>
         </footer>
