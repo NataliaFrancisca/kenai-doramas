d2c8d0e8106c3860c5a3ebe79c7363d6dcbd4b5d
NataliaFrancisca <nathifrancisca@gmail.com>
2022-06-29 12:11:55 -0300

    feat: removing the component details serie

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,47 +1,38 @@
 import React from "react";
 
+import { Button } from "../../styles/Elements";
+
 const Card = ({className, dataSerie}) => {
 
+    const {category, title, description, watch} = dataSerie;
+
     return(
-        <div className={className}>
-            <span>Categoria</span>
+        <article className={className}>
+            <span>{category[0]}</span>
             <div className="list-stars">
-                <img src={'img/icons/star-white.png'}/>
-                <img src={'img/icons/star-white.png'}/>
-                <img src={'img/icons/star-white.png'}/>
-                <img src={'img/icons/star-white.png'}/>
-                <img src={'img/icons/star-white.png'}/>
+                <img src="img/icons/rating.svg" alt="rating"/>
             </div>
 
-            <h2>{dataSerie.title}</h2>
+            <h2>{title}</h2>
 
-            <DetailsSerie details={dataSerie} />
-        </div>
+            <section className="details-serie">
+                <div className="list-category">
+                    {category.map((category, index) => (
+                        <span key={index}>{category}</span>
+                    ))}
+                </div>
 
+                <p>{description}</p>
+
+                <Button>
+                    <a href={watch} target="_blank">Watch Now</a>
+                </Button>
+            </section>
+        </article>
     )
 }
 
 export default Card;
 
 
-const DetailsSerie = ({details}) => {
 
-    const {description, category, watch} = details;
-
-    console.log(details)
-    return(
-        <div className="details-serie">
-            <div className="list-category">
-                {category.map((category, index) => (
-                    <span key={index}>{category}</span>
-                ))}
-            </div>
-
-            <p>{description}</p>
-
-            <button>
-                <a href={watch} target="_blank">Watch Now</a>
-            </button>
-        </div>
-    )
-}
