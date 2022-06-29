c1ac757fc69baae1d8a6074cf4a3abed294fc186
NataliaFrancisca <nathifrancisca@gmail.com>
2022-06-29 12:13:27 -0300

    style: change classes name

--- src/components/HighLight/HighLightStyled.js
+++ src/components/HighLight/HighLightStyled.js
@@ -7,65 +7,57 @@ export const HighLightStyled = styled(HighLight)`
     flex-direction: column;
     font-family: 'Opens Sans', sans-serif;
     color: #F9F9F9;
-    margin-top: 4rem;
-    padding: 0 1rem;
+   
+    .category{
+        margin: 25px 0;
+        color: #FFA634;
+        font-family: 'Open Sans';
+        font-weight: 600;
+        padding: 4px 8px;
+        background-color: #181818;
+        border-radius: 0px 8px;
+        width: min-content;
 
-        .header-category-list{
-            padding-left: .3rem;
-            margin: .5rem 0;
-
-            span{
-                margin: 1rem;
-                color: #FFA634;
-                font-family: 'Open Sans';
-                font-weight: 600;
-                padding: 4px 8px;
-                background-color: #181818;
-                border-radius: 0px 8px;
-            }
-
-            span:nth-child(1){
-                margin-left: 0rem;
-            }
+        &:nth-child(1){
+            margin-left: 0rem;
         }
+    }
+        
+    .stars{
+        display: flex;
+        margin-bottom: 20px;
 
-        .stars{
-            display: flex;
-            margin: .5rem 0;
-        }
-
-        .stars img{
+        img{
             height: 30px;
             width: 30px;
         }
+    }
 
+    h1{
+        font-weight: 600;
+        font-size: 3em;
+        margin-bottom: 20px;
+    }
+
+    p{
+        font-weight: 300;
+        font-size: 18px;
+        text-align: justify;
+        line-height: 28px;
+    }
+
+    button{
+        width: 286px;
+        margin-top: 40px;
+    }
+
+    @media only screen and (max-width: 800px) {
+        width: 100%;
+    }
+
+    @media only screen and (max-width: 500px) {
         h1{
-            font-weight: 600;
-            font-size: 3em;
-            margin: .5rem 0;
+            font-size: 40px;
         }
-
-        p{
-            font-weight: 400;
-            font-size: 1.2em;
-            text-align: justify;
-        }
-
-        button{
-            width: 286px;
-            background-color: #FFA634;
-            height: 56px;
-
-            color: black;
-            font-weight: 600;
-            font-size: 1em;
-
-            margin-top: 2rem;
-            border-radius: 4px;
-
-        }
-
-        @media only screen and (max-width: 800px) {
-            width: 100%;
-        }
+    }
 `
