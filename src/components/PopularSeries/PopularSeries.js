b1727ada8cba6e0e223ecd4fb6e7eb6be704230d
NataliaFrancisca <nathifrancisca@gmail.com>
2022-06-30 14:23:43 -0300

    feat: add carrousel images

--- src/components/PopularSeries/PopularSeries.js
+++ src/components/PopularSeries/PopularSeries.js
@@ -1,23 +1,113 @@
 import React from "react";
-
+import { useEffect, useState, useRef } from "react";
 import {db} from "../../db/data"
 
+import { Carrousel } from "./PopularSeriesStyled";
 import { CardStyled } from "../UI/CardStyled";
 
 const PopularSeries = ({className}) => {
 
+    const minPerSection = 4;
+
+    const [currentGroupShows, setCurrentGroupShows] = useState();
+    const [groupsShows, setGroupsShows] = useState([]);
+
+    const refButtonPrev = useRef();
+    const refButtonNext = useRef();
+
     const data = db.filter(serie => serie.id !== 0);
 
+    const createGroupOfShows = () => {
+        let min = 0;
+        let max = minPerSection;
+        let newId = 0;
+
+        const createGroup = () => {
+            const seriesGroups = {
+                id: newId,
+                series: data.slice(min, max)
+            }
+
+           
+            min += minPerSection;
+            max += minPerSection;
+            newId++;
+            setGroupsShows(prevState => [...prevState, seriesGroups])
+        }
+
+        while(min < data.length){
+            createGroup();
+            console.log(min)
+            console.log(max)
+        }
+    }
+
+    useEffect(() => {
+        return groupsShows.length < data.length / minPerSection ? createGroupOfShows() : false;
+    },[])
+
+    useEffect(() => {
+        const initalGroup = groupsShows.find(group => group.id == 0);
+        setCurrentGroupShows(initalGroup);
+
+        console.log(groupsShows)
+    },[groupsShows])
+
+    useEffect(() => {
+        const maxElements = groupsShows.length;
+
+        if(currentGroupShows){
+            currentGroupShows.id == maxElements - 1 ? refButtonNext.current.disabled = true : refButtonNext.current.disabled = false;
+            currentGroupShows.id == 0 ? refButtonPrev.current.disabled = true :  refButtonPrev.current.disabled = false;
+        }
+    })
+
+    const onNextShows = () => {
+        const maxElements = groupsShows.length;
+        
+        if(currentGroupShows.id == maxElements - 1){
+            refButtonNext.current.disabled = true;
+            return false;
+        }
+
+        const nextGroups = groupsShows.find(group => group.id == currentGroupShows.id + 1);
+        setCurrentGroupShows(nextGroups);
+    }
+
+    const onPreviousShows = () => {
+        if(currentGroupShows.id == 0){
+            refButtonPrev.current.disabled = true;
+            return false;
+        }
+
+        const nextGroups = groupsShows.find(group => group.id == currentGroupShows.id - 1);
+        setCurrentGroupShows(nextGroups);
+    }
+
     return(
-        <section className={className}>
+        <main className={className}>
             <h1>Populares</h1>
 
-            <div className="list">
-                {data.map(serie => (
-                  <CardStyled key={serie.id} imageBackground={serie.image} dataSerie={serie}/>
-                ))}
-            </div>
-        </section>
+            <Carrousel numberColumns={minPerSection}>
+                <button id="back-group" ref={refButtonPrev} onClick={() => onPreviousShows()}>
+                    <span className="material-symbols-outlined">chevron_left</span>
+                </button>
+
+                <section id="grid-section">
+                    {currentGroupShows ? 
+                        currentGroupShows.series.map(serie => (
+                            <CardStyled key={serie.id} imageBackground={serie.image} dataSerie={serie} />
+                        ))
+                        : null
+                    }
+                </section>
+
+                <button id="next-group" ref={refButtonNext} onClick={() => onNextShows()}>
+                    <span className="material-symbols-outlined">chevron_right</span>
+                </button>
+            </Carrousel>
+
+        </main>
     )
 }
 
--- src/components/PopularSeries/PopularSeriesStyled.js
+++ src/components/PopularSeries/PopularSeriesStyled.js
@@ -2,7 +2,6 @@ import styled from "styled-components"
 import PopularSeries from "./PopularSeries"
 
 export const PopularSeriesStyled = styled(PopularSeries)`
-    width: 100%;
     display: flex;
     flex-direction: column;
     font-family: 'Opens Sans', sans-serif;
@@ -10,6 +9,8 @@ export const PopularSeriesStyled = styled(PopularSeries)`
 
     margin-top: 9rem;
     margin-bottom: 2rem;
+
+    position: relative;
 
     h1{
         font-family: 'Opens Sans', sans-serif;
@@ -28,13 +29,64 @@ export const PopularSeriesStyled = styled(PopularSeries)`
     }
 
     .list{
-        margin-top: 1rem;
+        /* margin-top: 1rem;
 
         display: grid;
         align-items: center;
         justify-content: center;
         grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-        gap: 40px;
+        gap: 40px; */
     }
 
+`
+
+export const Carrousel = styled.section`
+    display: flex;
+    justify-content: space-between;
+
+    min-height: 600px;
+    height: auto;
+
+    position: relative;
+    scroll-behavior: smooth;
+    transition: 250ms all;
+    
+
+    #back-group, #next-group{
+        color: orange;
+        position: absolute;
+        top: 0px;
+        z-index: 111;
+        height: 100%;
+        width: 60px;
+        cursor: pointer;
+    }
+    
+    #back-group:disabled span,
+    #next-group:disabled span{
+        color: grey;
+    }
+
+    #back-group{
+        left: 0px;
+        background: linear-gradient(-90deg, rgba(0,0,0,0) 0%, rgba(0,0,0,.8) 130%);
+    }
+
+    #next-group{
+        right: 0px;
+        background: linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(0,0,0,.8) 130%);
+    }
+
+    #grid-section{
+        margin-top: 1rem;
+
+        display: grid;
+        align-items: center;
+        justify-content: start;
+      
+        width: 100%;
+        gap: 30px;
+        grid-template-columns: repeat(${props => props.numberColumns}, 1fr);
+        /* grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); */
+    }
 `
